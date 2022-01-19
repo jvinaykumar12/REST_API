f78fb05b0cdd467b79cffd62a10c64a4f3d6d04a
gravitylight <jvinaykumar12@gmail.com>
2022-01-19 18:48:45 +0530

    dsf

--- asyncWar.js
+++ asyncWar.js
@@ -10,3 +10,16 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(${imageLink.urls.full})`
         document.getElementById('authorname').innerHTML = `${imageLink.user.username}`
     })
+    .catch((err)=>{
+        console.log(err)
+        console.log('hi')
+        document.body.style.backgroundImage = `url("dutch.webp")`
+        document.getElementById('authorname').innerText = 'local'
+    })
+
+fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=INR")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.bitcoin.inr)
+        document.getElementById('crypto').innerText = 'crypto price = ' + data.bitcoin.inr + 'Rupees'
+    })
