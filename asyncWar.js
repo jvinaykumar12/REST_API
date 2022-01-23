f59b3be1ccda4ca062032070c5469b1662b4922b
gravity <jvinaykumar12@gmail.com>
2022-01-23 13:08:32 +0530

    a

--- asyncWar.js
+++ asyncWar.js
@@ -1,25 +1,36 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.urls)
-        return new Promise((resolve)=>{
-            resolve(data)
-        })
+    .then(response => {
+        if(!response.ok){
+            throw Error("hello iam error")
+        }
+        return response.json()
     })
     .then(imageLink => {
         document.body.style.backgroundImage = `url(${imageLink.urls.full})`
         document.getElementById('authorname').innerHTML = `${imageLink.user.username}`
     })
-    .catch((err)=>{
+    .catch((err)=> {
+        console.log('hi1')
         console.log(err)
-        console.log('hi')
         document.body.style.backgroundImage = `url("dutch.webp")`
         document.getElementById('authorname').innerText = 'local'
     })
 
-fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=INR")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.bitcoin.inr)
-        document.getElementById('crypto').innerText = 'crypto price = ' + data.bitcoin.inr + ' in Rupees hello '
+// fetch("https://api.coingecko.com/api/v3/simple/price?ids=sfdg&vs_currencies=inr")
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(response => {
+        if(!response.ok){
+            throw Error("hello iam error")
+        }
+        return response.json()
     })
+    .then(data => { 
+        console.log("hh")
+        console.log(data)
+        // document.getElementById('crypto').innerText = 'crypto price = ' + data.dogecoin.inr + ' in Rupees hello '
+    })
+    .catch(error => {
+        console.log("hi2")
+        console.log(error)
+        console.log("unable to find the given address")
+    }) 
